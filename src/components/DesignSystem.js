94fe9082b6c73c735dd2aeb5a786d700f2e1c6dc
Asha Cole <maymunahrose@gmail.com>
2022-11-01 13:51:49 -0400

    finished design system

---
+++ src/components/DesignSystem.js
@@ -0,0 +1,43 @@
+import ProfilePic from "../profile-images/image-amyrobson.webp";
+import ReplyButton from "./Icons/ReplyButton";
+import EditButton from "./Icons/EditButton";
+import DeleteButton from "./Icons/DeleteButton";
+import PlusIcon from "./Icons/PlusIcon";
+import MinusIcon from "./Icons/MinusIcon";
+import classes from "../styles/_DesignSystem.module.scss";
+
+const DesignSystem = () => {
+  return (
+    <div className={classes.designSystem}>
+      <h1>Design System</h1>
+      <div className={classes.commentInfo}>
+        <img src={ProfilePic} alt="" className={classes.profilePic} />
+        <p className={classes.commentor}>amyrobson</p>
+        <p className={classes.timestamp}>1 month ago</p>
+      </div>
+      <p className={classes.tag}>@maxblagun</p>
+      <div className={classes.card}>
+        <p className={classes.comment}>
+          Impressive! Though it seems the drag feature could be improved. But
+          overall it looks incredible. You've nailed the design and the
+          responsiveness at various breakpoints works really well.
+        </p>
+      </div>
+      <ReplyButton />
+      <EditButton />
+      <DeleteButton />
+      <button className={classes.button}>Reply</button>
+      <div className={classes.likesBar}>
+        <span className={classes.like}>
+          <PlusIcon />
+        </span>
+        <span className={classes.amountOfLikes}>4</span>
+        <span className={classes.dislike}>
+          <MinusIcon/>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default DesignSystem;
---
+++ src/components/Icons/ReplyButton.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+const ReplyButton = () => {
+  const [color, setColor] = useState(
+  "hsl(238, 40%, 52%)");
+  const hoverColorHandler = () => {
+    setColor("hsl(239 57% 85%)");
+  };
+  const resetColorHandler = () => {
+    setColor("hsl(238, 40%, 52%)");
+  };
+
+  return (
+    <div onMouseOver={hoverColorHandler} onMouseLeave={resetColorHandler} className="icon">
+      <span>
+      <svg width="14" height="13" xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M.227 4.316 5.04.16a.657.657 0 0 1 1.085.497v2.189c4.392.05 7.875.93 7.875 5.093 0 1.68-1.082 3.344-2.279 4.214-.373.272-.905-.07-.767-.51 1.24-3.964-.588-5.017-4.829-5.078v2.404c0 .566-.664.86-1.085.496L.227 5.31a.657.657 0 0 1 0-.993Z"
+          fill={color}
+        />
+        
+      </svg>
+      </span>
+
+      Reply
+    </div>
+  );
+};
+
+export default ReplyButton;
